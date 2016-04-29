a5ef3784341b0bc43b32792aaebaafbc0651330d
jmvkayitare@yahoo.com <jmvkayitare@yahoo.com>
2016-04-30 02:10:22 +0300

    added login

--- app/src/main/java/com/example/jaykayitare/mezea/LoginActivity.java
+++ app/src/main/java/com/example/jaykayitare/mezea/LoginActivity.java
@@ -1,13 +1,71 @@
 package com.example.jaykayitare.mezea;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.Result;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.TwitterSession;
+import com.twitter.sdk.android.core.identity.TwitterLoginButton;
+
+import io.fabric.sdk.android.Fabric;
 
 public class LoginActivity extends AppCompatActivity {
+
+    // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
+    private static final String TWITTER_KEY = "yGhy4c9S3Oy3laXHISaGeuZLq";
+    private static final String TWITTER_SECRET = "COTzl4B7SUkUiTiF2GzodHpeVtFmrXglG4SxfxqFxYHdnEwZer";
+
+    private TwitterLoginButton loginButton;
+    private TextView status;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+        Fabric.with(this, new Twitter(authConfig));
+
         setContentView(R.layout.activity_login);
+
+        loginButton = (TwitterLoginButton)findViewById(R.id.twitter_login_button);
+        loginButton.setCallback(new LoginHandler());
+        status = (TextView)findViewById(R.id.status);
+//        status.setText("Status: Ready");
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        loginButton.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private class LoginHandler extends Callback<TwitterSession> {
+        @Override
+        public void success(Result<TwitterSession> twitterSessionResult) {
+
+//            String output = "Status: " +
+//                    "Your login was successful " +
+//                    twitterSessionResult.data.getUserName() +
+//                    "\nAuth Token Received: " +
+//                    twitterSessionResult.data.getAuthToken().token;
+//
+//            status.setText(output);
+            Intent intent = new Intent(getApplicationContext(), LocationFeed.class);
+            startActivity(intent);
+
+        }
+
+        @Override
+        public void failure(TwitterException e) {
+
+            status.setText("Status: Login Failed");
+
+        }
     }
 }
