05271d8b936278b6a212adc48e0984f16adcd170
jmvkayitare@yahoo.com <jmvkayitare@yahoo.com>
2016-04-29 23:17:41 +0300

    Added fabric

--- app/src/main/java/com/example/jaykayitare/mezea/LocationFeed.java
+++ app/src/main/java/com/example/jaykayitare/mezea/LocationFeed.java
@@ -1,13 +1,16 @@
 package com.example.jaykayitare.mezea;
 
+import android.app.ListActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class LocationFeed extends AppCompatActivity {
+public class LocationFeed extends ListActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location_feed);
+
+
     }
 }
