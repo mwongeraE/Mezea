325b05f59a5298402c12e5a3a4e1aa331679cd19
mwongeraE <evansmwongera@gmail.com>
2016-04-30 03:18:37 +0300

    timeline mezea

--- app/src/main/java/com/example/jaykayitare/mezea/SplashScreen.java
+++ app/src/main/java/com/example/jaykayitare/mezea/SplashScreen.java
@@ -3,13 +3,23 @@ package com.example.jaykayitare.mezea;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import io.fabric.sdk.android.Fabric;
 
 public class SplashScreen extends Activity {
+
+    // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
+    private static final String TWITTER_KEY = "yGhy4c9S3Oy3laXHISaGeuZLq";
+    private static final String TWITTER_SECRET = "COTzl4B7SUkUiTiF2GzodHpeVtFmrXglG4SxfxqFxYHdnEwZer";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         // TODO Auto-generated method stub
         super.onCreate(savedInstanceState);
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+        Fabric.with(this, new Twitter(authConfig));
         setContentView(R.layout.activity_splash_screen);
 
         Thread timerThread = new Thread(){
---
+++ app/src/main/java/com/example/jaykayitare/mezea/TimelineActivity.java
@@ -0,0 +1,28 @@
+package com.example.jaykayitare.mezea;
+
+/**
+ * Created by mwongera on 4/30/16.
+ */
+
+import android.app.ListActivity;
+import android.os.Bundle;
+
+import com.twitter.sdk.android.tweetui.TweetTimelineListAdapter;
+import com.twitter.sdk.android.tweetui.UserTimeline;
+
+public class TimelineActivity extends ListActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.timeline);
+
+        final UserTimeline userTimeline = new UserTimeline.Builder()
+                .screenName("mezea_")
+                .build();
+        final TweetTimelineListAdapter adapter = new TweetTimelineListAdapter.Builder(this)
+                .setTimeline(userTimeline)
+                .build();
+        setListAdapter(adapter);
+    }
+}
